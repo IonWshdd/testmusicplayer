764656ac4b8c5b6ad1b9335ccc87129efc417596
IonWshdd <gmachael10@gmail.com>
2025-03-21 22:03:24 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className='flex flex-col items-center justify-center w-full min-h-screen p-4 bg-gray-800 text-white'>
             <div className='text-xl justify-center text-center lg:text-center'>Welcome to Kanye West Music Player, the best music player for Unreleased Kanye West</div>
-            <div className='justify-center'><Link to="/track" className='text-2xl font-bold underline'>Listen Now</Link></div>
+            <div className='justify-center'><Link to="/bully" className='text-2xl font-bold underline'>Listen Now</Link></div>
             <div className='absolute bottom-8 left-0 p-4'>
                 <TotalSongsScript />
             </div>
