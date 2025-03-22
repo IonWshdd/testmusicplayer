ed726c132f396b24ca059880e1577782197cc682
IonWshdd <gmachael10@gmail.com>
2025-03-21 22:32:03 -0500

    Create bullysongs.js

---
+++ src/constants/bullysongs.js
@@ -0,0 +1,92 @@
+export const songsData = [
+    {
+        song_name: "PREACHER MAN",
+        song_length: "3:06",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/PREACHER%20MAN",
+        cover_image_url: "https://lh7-rt.googleusercontent.com/sheetsz/AHOq17FBjw5DT9RXwgUgAaYXxQAn4XyLOv_w_39DSTTaqRtewYIFnDSZMwa_cxUZkdAbAEcdea2qAN29DHZ9e6-l27UG18lrnAKXEg0aH_kTsEMGKcSBTTS-HJ6THtYc3wxL?key=vFIW87Iq3I6Uh5WJGSWtug"
+    },
+    {
+        song_name: "I CANT BEAR",
+        song_length: "1:56",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/I%20CANT%20BEAR",
+        cover_image_url: ""
+    },
+    {
+        song_name: "CLOSE TO YOU",
+        song_length: "3:03",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/CLOSE%20TO%20YOU",
+        cover_image_url: ""
+    },
+    {
+        song_name: "CIRCLES",
+        song_length: "2:46",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/CIRCLES",
+        cover_image_url: ""
+    },
+    {
+        song_name: "BULLY",
+       song_length: "2:46",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/BULLY",
+        cover_image_url: ""
+    },
+    {
+        song_name: "SPANISH SONG",
+        song_length: "2:08",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/SPANISH%20SONG",
+        cover_image_url: ""
+    },
+    {
+        song_name: "BEAUTY AND THE BEAST",
+        song_length: "1:39",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/BEAUTY%20AND%20THE%20BEAST",
+        cover_image_url: ""
+    },
+    {
+        song_name: "SHOWTIME",
+        song_length: "3:24",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/SHOWTIME",
+        cover_image_url: ""
+    },
+    {
+        song_name: "HIGHS AND LOWS",
+        song_length: "1:51",
+        artist_name: "Kanye West",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/HIGHS%20AND%20LOWS",
+        cover_image_url: ""
+    },
+    {
+        song_name: "MELROSE",
+        song_length: "3:41",
+        artist_name: "Kanye West, Ty Dolla $ign, Playboi Carti",
+        movie_name: "",
+        genre: "hip-hop",
+        song_url: "https://res.cloudinary.com/dywggoioe/video/upload/f_auto:video,q_auto/MELROSE",
+        cover_image_url: ""
+    }
+];
